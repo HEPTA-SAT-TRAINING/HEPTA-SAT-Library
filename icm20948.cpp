7a9a266038a285c5d5099a9fc8de5acdc56bf189
masa-night <csms21050@g.nihon-u.ac.jp>
2025-01-21 21:18:07 +0900

    磁気センサリファクタリング実施（できればdelay減らしたい）

--- icm20948.cpp
+++ icm20948.cpp
@@ -63,48 +63,9 @@ void Icm20948::get_gyro(float *gx, float *gy, float *gz) {
 void Icm20948::get_mag(float *mx, float *my, float *mz) {
   uint8_t buf[6] = {0};
 
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_WHO_AM_I);
-  Wire.endTransmission();
-
-  Wire.requestFrom(AK09916_I2C_ADDR, 1);
-  if(Wire.available()) {
-    Serial.print("WAI: ");
-    Serial.println(Wire.read(), HEX);
-  }
-
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_XAXIS_HIGH);
-  Wire.endTransmission();
-
-  Wire.requestFrom(AK09916_I2C_ADDR, 6);
-  if(Wire.available()) {
-    for(uint8_t i = 0; i < 6; i++) {
-      buf[i] = Wire.read();
-    }
-  }
-
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_CNTL_2);
-  // Wire.write((uint8_t)count%3);
-  Wire.write(1);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_CNTL_2);
-  Wire.endTransmission();
-
-  Wire.requestFrom(AK09916_I2C_ADDR, 1);
-  if(Wire.available()) {
-    Serial.print("CNTL2: ");
-    Serial.println(Wire.read(), HEX);
-  }
-  // _select_bank(BANK0);
-  // _reg_read(ICM20948_EXT_SLV_SENS_DATA_00, buf, 6);
-
-  // uint16_t wai = _who_am_i_mag();
-  // Serial.print("mag who am i: ");
-  // Serial.println(wai);
+  _ak09916_reg_write(AK09916_CNTL_2, (uint8_t)AK09916_TRIGGER_MODE);
+  delay(10);
+  _ak09916_reg_read(AK09916_XAXIS_HIGH, buf, 6);
 
   int16_t mx_int = (int16_t)(buf[1] << 8 | buf[0]);
   int16_t my_int = (int16_t)(buf[3] << 8 | buf[2]);
@@ -219,59 +180,10 @@ void Icm20948::_reg_read(uint8_t reg, uint8_t val[], uint8_t len) {
 }
 
 void Icm20948::_init_mag(void) {
-  // bool is_success = false;
-  // _enable_i2c_master();
-
-  // _mag_reset();
-  // _reset();
-  // _wakeup();
-
-  // _select_bank(BANK2);
-  // _reg_write(ICM20948_ODR_ALIGN_EN, 1);
-
-  // for(uint8_t i = 0; i < 10; i++) {
-  //   delay(10);
-  //   _enable_i2c_master();
-  //   delay(10);
-
-  //   uint16_t wai = _who_am_i_mag();
-  //   Serial.print("mag who am i: ");
-  //   Serial.println(wai);
-
-  //   if(wai == AK09916_DEVICE_ID) {
-  //     is_success = true;
-  //     break;
-  //   } else {
-  //     _i2c_master_reset();
-  //   }
-  // }
-  // if(is_success) {
-  //   _set_mag_mode(AK09916_CONT_MODE_20HZ);
-  // }
   _select_bank(BANK0);
-  _reg_write(ICM20948_USER_CTRL, 0<<5);
-  _reg_write(0x0F, 1<<1);
-
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_WHO_AM_I);
-  Wire.endTransmission();
-
-  Wire.requestFrom(AK09916_I2C_ADDR, 1);
-  if(Wire.available()) {
-    Serial.print("WAI: ");
-    Serial.println(Wire.read(), HEX);
-  }
-
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_CNTL_3);
-  Wire.write(1);
-  Wire.endTransmission();
-  delay(10);
-
-  Wire.beginTransmission(AK09916_I2C_ADDR);
-  Wire.write(AK09916_CNTL_2);
-  Wire.write(4);
-  Wire.endTransmission();
+  _reg_write(ICM20948_INT_PIN_CFG, ICM20948_BYPASS_EN);
+  _mag_reset();
+  _ak09916_reg_write(AK09916_CNTL_2, (uint8_t)AK09916_CONT_MODE_100HZ);
 
   _select_bank(BANK2);
   _reg_write(ICM20948_ODR_ALIGN_EN, 1);
@@ -304,36 +216,35 @@ void Icm20948::_mag_reset(void) {
   delay(100);
 }
 
-void Icm20948::_set_mag_mode(AK09916_OP_MODE mode) {
-  _ak09916_reg_write(AK09916_CNTL_2, mode);
-  delay(10);
-  if(mode!=AK09916_PWR_DOWN){
-    _enable_mag_data_read(AK09916_XAXIS_HIGH, 0x08);
-  }
-}
-
-void Icm20948::_enable_mag_data_read(uint8_t reg, uint8_t len) {
-  _select_bank(BANK3);
-  _reg_write(ICM20948_I2C_SLV0_ADDR, AK09916_I2C_ADDR | AK09916_READ);
-  _reg_write(ICM20948_I2C_SLV0_REG, reg);
-  _reg_write(ICM20948_I2C_SLV0_CTRL, ICM20948_I2C_SLVX_EN | len);
-  delay(10);
-}
-
 void Icm20948::_ak09916_reg_write(uint8_t reg, uint8_t val) {
-  _select_bank(BANK3);
-  _reg_write(ICM20948_I2C_SLV4_ADDR, AK09916_I2C_ADDR);
-  _reg_write(ICM20948_I2C_SLV4_DO, val);
-  _reg_write(ICM20948_I2C_SLV4_REG, reg);
-  _reg_write(ICM20948_I2C_SLV4_CTRL, ICM20948_I2C_SLVX_EN);
-  while(_reg_read(ICM20948_I2C_SLV4_CTRL) & ICM20948_I2C_SLVX_EN);
+  Wire.beginTransmission(AK09916_I2C_ADDR);
+  Wire.write(reg);
+  Wire.write(val);
+  Wire.endTransmission();
 }
 
 uint8_t Icm20948::_ak09916_reg_read(uint8_t reg) {
-  _select_bank(BANK3);
-  _reg_write(ICM20948_I2C_SLV4_ADDR, AK09916_I2C_ADDR | AK09916_READ); // read AK09916
-  _reg_write(ICM20948_I2C_SLV4_REG, reg); // define AK09916 register to be read
-  _reg_write(ICM20948_I2C_SLV4_CTRL, ICM20948_I2C_SLVX_EN);
-  while(_reg_read(ICM20948_I2C_SLV4_CTRL) & ICM20948_I2C_SLVX_EN){;}
-  return _reg_read(ICM20948_I2C_SLV4_DI);
+  uint8_t ret;
+  Wire.beginTransmission(AK09916_I2C_ADDR);
+  Wire.write(reg);
+  Wire.endTransmission();
+
+  Wire.requestFrom(AK09916_I2C_ADDR, 1);
+  if(Wire.available()) {
+    ret = Wire.read();
+  }
+  return ret;
+}
+
+void Icm20948::_ak09916_reg_read(uint8_t reg, uint8_t val[], uint8_t len) {
+  Wire.beginTransmission(AK09916_I2C_ADDR);
+  Wire.write(reg);
+  Wire.endTransmission();
+
+  Wire.requestFrom(AK09916_I2C_ADDR, len);
+  if(Wire.available()) {
+    for(uint8_t i = 0; i < len; i++) {
+      val[i] = Wire.read();
+    }
+  }
 }
