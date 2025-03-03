91e73f460c9299fb90a341298fb09f7b1ae902a5
masa-night <csms21050@g.nihon-u.ac.jp>
2025-03-03 15:32:24 +0900

    GNSSライブラリ動作確認

--- hepta_library.ino
+++ hepta_library.ino
@@ -4,19 +4,29 @@
 #include "src/com/hepta_com.h"
 
 #include <camera_c1098.h>
+#include <gps_gp1818mk.h>
 
 HeptaSensor sensor;
 CameraC1098 cam;
+Gps1818mk gps;
 
 void setup() {
   Serial.begin(9600);
+  Serial1.setFIFOSize(128);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  Serial.println("Camera");
-  if(cam.begin(C1098_BAUD_RATE_115200, C1098_JPEG_SIZE_VGA)) {
-    cam.take_picture();
+  float lat, lon, alt;
+  // while(gps.is_data_available());
+  if(gps.get_position(&lat, &lon, &alt)) {
+    Serial.println("-------------------");
+    Serial.print("Lat: ");
+    Serial.println(lat, 6);
+    Serial.print("Lon: ");
+    Serial.println(lon, 6);
+    Serial.print("Alt: ");
+    Serial.println(alt, 1);
+    Serial.println("-------------------");
   }
-
-  delay(1000);
 }
